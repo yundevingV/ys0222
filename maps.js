c6225c8e6882095f17e40a0e796f94cd408576a3
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-26 00:58:26 +0900

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,6 +1,6 @@
 var mapContainer1 = document.getElementById('map1'),
     mapOption1 = {  
-        center: new daum.maps.LatLng(33.450701, 126.570667),
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 3
     };
 var map1 = new kakao.maps.Map(mapContainer1, mapOption1);
@@ -16,7 +16,8 @@ var map2 = new kakao.maps.Map(mapContainer2, mapOption2);
 
 var mapContainer3 = document.getElementById('map3'),
     mapOption3 = { 
-        center: new daum.maps.LatLng(37.449299, 126.703753),
+        center: new kakao.maps.LatLng(37.449299, 126.703753),
         level: 3
     };
 var map3 = new kakao.maps.Map(mapContainer3, mapOption3);
+
