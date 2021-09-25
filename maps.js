912417840c2d73fbbf778db6989cfbe797bc2ae0
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-26 00:05:46 +0900

    Update maps.js

--- maps.js
+++ maps.js
@@ -12,3 +12,17 @@
 	$(function(){
 		kakaomap();
 	})
+
+	function kakaomap1(){
+		var container = document.getElementById('map1');
+		var options = {
+			center: new kakao.maps.LatLng(55.450701, 126.570667),
+			level: 3
+		};
+
+		var map1 = new kakao.maps.Map1(container, options);
+	};
+
+	$(function(){
+		kakaomap1();
+	})
