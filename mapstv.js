65af644a21415bc5365a5ab1a57bfc49afe65b21
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-26 01:46:50 +0900

    Create mapstv.js

---
+++ mapstv.js
@@ -0,0 +1,6 @@
+var mapContainer3 = document.getElementById('map3'),
+    mapOption3 = { 
+        center: new kakao.maps.LatLng(37.449299, 126.703753),
+        level: 3
+    };
+var map3 = new kakao.maps.Map(mapContainer3, mapOption3);
