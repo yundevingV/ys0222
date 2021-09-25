647a8a77b40b24934480a56c31aa25d6b0054a25
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-26 00:17:52 +0900

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,28 +1,15 @@
+var mapContainer1 = document.getElementById('map1'),
+    mapOption1 = {  
+        center: new daum.maps.LatLng(33.450701, 126.570667),
+        level: 3
+    };
+var map1 = new daum.maps.Map(mapContainer1, mapOption1);
+    
 
-	function kakaomap(){
-		var container = document.getElementById('map');
-		var options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3
-		};
 
-		var map = new kakao.maps.Map(container, options);
-	};
-
-	$(function(){
-		kakaomap();
-	})
-
-	function kakaomap1(){
-		var container1 = document.getElementById('map1');
-		var options1 = {
-			center: new kakao.maps.LatLng(55.450701, 126.570667),
-			level: 3
-		};
-
-		var map1 = new kakao.maps.Map1(container1, options1);
-	};
-
-	$(function(){
-		kakaomap1();
-	})
+var mapContainer2 = document.getElementById('map2'),
+    mapOption2 = { 
+        center: new daum.maps.LatLng(33.450701, 126.570667),
+        level: 3
+    };
+var map2 = new daum.maps.Map(mapContainer2, mapOption2);
