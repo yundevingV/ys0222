b500dd1becc20fa274b285dca2a1235b87f1ad29
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-25 23:48:45 +0900

    Create kakaompa.js

---
+++ kakaompa.js
@@ -0,0 +1,14 @@
+
+	function kakaomap(){
+		var container = document.getElementById('map');
+		var options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+
+		var map = new kakao.maps.Map(container, options);
+	};
+
+	$(function(){
+		kakaomap();
+	})
