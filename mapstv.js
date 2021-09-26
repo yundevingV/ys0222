04fb8e33599daa7c47f491b532ec6179b1374bc7
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-26 23:39:15 +0900

    Update mapstv.js

--- mapstv.js
+++ mapstv.js
@@ -5,13 +5,33 @@ var mapContainer3 = document.getElementById('map3'),
     };
 var map3 = new kakao.maps.Map(mapContainer3, mapOption3);
 
-// 마커가 표시될 위치입니다 
+
 var markerPosition3  = new kakao.maps.LatLng(37.449299, 126.703753); 
 
-// 마커를 생성합니다
+
 var marker3 = new kakao.maps.Marker({
     position: markerPosition3
 });
 
-// 마커가 지도 위에 표시되도록 설정합니다
+
 marker3.setMap(map3);
+
+
+
+var mapContainer4 = document.getElementById('map4'),
+    mapOption4 = { 
+        center: new kakao.maps.LatLng(37.55037139421805, 126.92009609173007),
+        level: 3
+    };
+var map4 = new kakao.maps.Map(mapContainer3, mapOption3);
+
+
+var markerPosition4  = new kakao.maps.LatLng(37.55037139421805, 126.92009609173007); 
+
+
+var marker4 = new kakao.maps.Marker({
+    position: markerPosition4
+});
+
+
+marker3.setMap(map4);
