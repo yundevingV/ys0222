31ca1d9995c2eefa5d6918b8c9ed0a815705127a
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-09-26 23:58:01 +0900

    Update mapstv.js

--- mapstv.js
+++ mapstv.js
@@ -18,3 +18,20 @@ marker3.setMap(map3);
 
 
 
+var mapContainer4 = document.getElementById('map4'),
+    mapOption4 = { 
+        center: new kakao.maps.LatLng(37.55037139421805, 126.92009609173007),
+        level: 3
+    };
+var map4 = new kakao.maps.Map(mapContainer4, mapOption4);
+
+
+var markerPosition4  = new kakao.maps.LatLng(37.55037139421805, 126.92009609173007); 
+
+
+var marker4 = new kakao.maps.Marker({
+    position: markerPosition4
+});
+
+
+marker4.setMap(map4);
