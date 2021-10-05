b0e995f171528e9555c0d1d769f7ff94a0a10c84
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-10-05 19:41:26 +0900

    Update hanrivermap.js

--- hanrivermap.js
+++ hanrivermap.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), 
     mapOption = { 
         center: new kakao.maps.LatLng(37.51915,126.98310), 
-        level: 5
+        level: 8
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); 
@@ -37,3 +37,62 @@ var marker = new kakao.maps.Marker({
 
 marker.setMap(map);
 //미니스톱 양화 1호점
+    
+var markerPosition = new kakao.maps.LatLng(37.53292,126.92397); 
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+//씨스페이스 24 한강 여의도 4호점
+    
+ var markerPosition = new kakao.maps.LatLng(37.53299,126.92313); 
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+//이마트24 여의도4호점
+    
+     var markerPosition = new kakao.maps.LatLng(37.52809,126.93167); 
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+//씨유 한강 여의도 2호점
+    
+         var markerPosition = new kakao.maps.LatLng(37.52688,126.93310); 
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+//씨유 한강 여의도 카페점
+var markerPosition = new kakao.maps.LatLng(37.52635,126.93346); 
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+//미니스톱 한강 여의나루점
+    
+    var markerPosition = new kakao.maps.LatLng(37.52629,126.93359); 
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+//씨유 한강 여의도점
