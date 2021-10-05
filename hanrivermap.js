768f98fdd8a3173986ec0e44d45d060d075e2cef
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-10-05 19:30:39 +0900

    Update hanrivermap.js

--- hanrivermap.js
+++ hanrivermap.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), 
     mapOption = { 
         center: new kakao.maps.LatLng(37.51915,126.98310), 
-        level: 10 
+        level: 5
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); 
@@ -18,21 +18,21 @@ var marker = new kakao.maps.Marker({
 marker.setMap(map);
 //gs한강 양화 3호점
 
-var markerPosition1  = new kakao.maps.LatLng(37.53978,126.89978); 
+var markerPosition  = new kakao.maps.LatLng(37.53978,126.89978); 
 
 
-var marker1 = new kakao.maps.Marker({
-    position: markerPosition1
+var marker = new kakao.maps.Marker({
+    position: markerPosition
 });
 
 marker.setMap(map);
 //gs한강 양화 1호점
 
-var markerPosition2 = new kakao.maps.LatLng(37.53764,126.90411); 
+var markerPosition = new kakao.maps.LatLng(37.53764,126.90411); 
 
 
-var marker2 = new kakao.maps.Marker({
-    position: markerPosition2
+var marker = new kakao.maps.Marker({
+    position: markerPosition
 });
 
 marker.setMap(map);
