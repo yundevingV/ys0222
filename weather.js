feb3da20cf398b736574e8d88a589d9807828b81
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-10-09 03:44:14 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,21 +2,11 @@ $.getJSON('http://api.openweathermap.org/data/2.5/forecast?id=1835848&APPID=0ef0
 ,function(data){
 var $minTemp = data.list[0].main.temp_min;
 var $maxTemp = data.list[0].main.temp_max;
-
 var $humidity = data.list[0].main.humidity;
-
 var $probability = data.list[0].clouds.all;
 var $speed = data.list[0].wind.speed;
 var $deg = data.list[0].wind.deg;
 
-
-
-if($type == "Clouds")
-$type = "구름";
-else if($type == "Rain")
-$type = "비";
-else
-$type = "맑음";
 
 $('.clowtemp').append($minTemp + "°C");
 $('.chightemp').append($maxTemp + "°C");
