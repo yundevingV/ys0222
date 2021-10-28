12a87f53208b90a05204a1598b4a8ccfdc55649d
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-10-28 13:39:36 +0900

    Update timer.js

--- JS/timer.js
+++ JS/timer.js
@@ -3,4 +3,7 @@ let timer = 0;
 let x =setInterval(function(){
   timer =timer +9;
   document.getElementById("demo").innerHTML = timer;
+  if (timer >500)
+  {timer =timer +5;
+    document.getElementById("demo").innerHTML = timer;}
 }, 1000);
