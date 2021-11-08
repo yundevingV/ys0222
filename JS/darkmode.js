5305dad1f30fbf49880423b4f147293b1ca12d18
ys0222 <91381230+ys0222@users.noreply.github.com>
2021-11-08 11:18:50 +0900

    Update darkmode.js

--- JS/darkmode.js
+++ JS/darkmode.js
@@ -1,35 +1,31 @@
+var subs = document.getElementById("converter")
+
 var changeBodyColor={
-    setBodyColor: function(_bodyColor){
-        document.querySelector("body").style.color=_bodyColor;
+    setBodyColor: function(bodyColor){
+        document.querySelector("body").style.color=bodyColor;
     },
-    setBodyBackColor: function(_bodyBackColor){
-        document.querySelector("body").style.backgroundColor=_bodyBackColor;
+    setBodyBackColor: function(bodyBackColor){
+        document.querySelector("body").style.backgroundColor=bodyBackColor;
 },
 };
-
+//changeBodyColor 객체 생성
 
 function convertDayNight(Self){
-    if(self.value=="day"){
-        self.value="night";
+    if(self.value=="night"){
+        self.value="day";
         changeBodyColor.setBodyBackColor("#282828");
         changeBodyColor.setBodyColor("white");
-       
-        
+        if(subs.innerText === 'Veiw Dark Mode') {
+        subs.innerText = 'Veiw Light Mode';
+          } else subs.innerText ='Veiw Dark Mode';
+
     }
     else {
-        self.value="day";
+        self.value="night";
         changeBodyColor.setBodyBackColor("white");
         changeBodyColor.setBodyColor("black");
-        
-     
-        
+        if(subs.innerText === 'Veiw Light Mode') {
+        subs.innerText = 'Veiw Dark Mode';
+         } else subs.innerText ='Veiw Light Mode';
     }
 }
-
-const subs = document.getElementById("converter")
-
-subs.addEventListener("click", function() {
-    if(subs.innerText === 'Veiw Dark Mode') {
-        subs.innerText = 'Veiw Light Mode';
-    } else subs.innerText ='Veiw Dark Mode';
-});
